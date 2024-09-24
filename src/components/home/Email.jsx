8c075f902f875dd25fab5168606bb379dd2d73c1
Apple <sndpjaat008@gmail.com>
2024-09-24 14:55:57 +0530

    add border

--- src/components/home/Email.jsx
+++ src/components/home/Email.jsx
@@ -23,14 +23,14 @@ export const Email = () => {
   };
   return (
     <form ref={form} onSubmit={sendEmail}>
-     <div className='flex items-center justify-center h-screen  gap-5'>
-     <label className=''>Name</label>
-      <input type="text" name="user_name" /> 
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+     <div className='flex items-center flex-col justify-center h-screen gap-4'>
+     <label className=" border-transparent bg-black text-white px-2 py-1 text-4xl rounded transition duration-300 border-[2px] hover:border-blue-500 hover:bg-red-800">Name</label>
+        <input type="text" name="user_name" className="border border-yellow-500 bg-gray-100 text-3xl px-3 py-2 rounded transition duration-300 focus:border-blue-500 focus:bg-white" placeholder="Enter your name" />
+      <label className='border-transparent bg-black text-white px-2 py-1 text-4xl rounded transition duration-300 border-[2px] hover:border-blue-500 hover:bg-red-800'>Email</label>
+      <input className='border border-yellow-500 bg-gray-100 text-3xl px-3 py-2 rounded transition duration-300 focus:border-blue-500 focus:bg-white' type="email" name="user_email" />
+      <label className='border-transparent bg-black text-white px-2 py-1 text-4xl  rounded transition duration-300 border-[2px] hover:border-blue-500 hover:bg-red-800'>Message</label>
+      <textarea className='border border-yellow-500 bg-gray-100 text-3xl px-3 py-2 rounded transition duration-300 focus:border-blue-500 focus:bg-white' name="message" />
+      <input className='border-transparent bg-black text-white px-2 py-1 text-4xl  rounded transition duration-300 border-[2px] hover:border-blue-500 hover:bg-red-800' type="submit" value="Send" />
      </div>
     </form>
   );
